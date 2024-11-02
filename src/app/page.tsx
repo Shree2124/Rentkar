c38749f49bcb344bfb769cf2948ff740e979acab
Shree2124 <shreealasande@gmail.com>
2024-11-02 14:18:41 +0530

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home page
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/optimize");
+  }, [router]);
+  return <div>Home page</div>;
 }
